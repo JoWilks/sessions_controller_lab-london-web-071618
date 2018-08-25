ef397067a285ff6b34d15071b9308c99161a0311
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-08-25 08:39:36 +0000

    automatically backed up by learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+def new
+end
+
+def create
+  session[:name] = params[:name]
+  if session[:name] == nil
+    redirect_to '/'
+  else 
+  end
+end
+
+def destroy
+  session.delete :username
+end
+
+end
